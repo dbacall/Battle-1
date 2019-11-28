0b4827010a8731b936a552c06e88872c16892cb9
Student <student@Makerss-MacBook-Air.local>
2019-11-28 14:32:30 +0000

    POST/redirect to battle

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+enable :sessions
 
   get '/' do
     erb :index
   end
-  post '/battle' do
-    p params['Player 1']
-    @player1 = params['Player 1']
-    @player2 = params['Player 2']
+
+  post '/names' do
+    session[:player1] = params['Player 1']
+    session[:player2] = params['Player 2']
+
+    redirect '/battle'
+  end
+
+  get '/battle' do
+    @player1 = session[:player1]
+    @player2 = session[:player2]
     erb :battle
   end
 end
